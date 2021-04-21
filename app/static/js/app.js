79029d1f465a14aa43ab9275e38e3b37bfe3d09c
majesty101 <jody.harrison32@gmail.com>
2021-04-21 13:24:14 -0500

    Edited

--- app/static/js/app.js
+++ app/static/js/app.js
@@ -80,7 +80,7 @@ const NewsList = {
     fetch('https://newsapi.org/v2/top-headlines?country=us',
     {
       headers: { 
-        'Authorization': 'Bearer ---'
+        'Authorization': '9ee3b36c357c4566935a76191387af05'
       }
     })
 
@@ -103,7 +103,7 @@ const NewsList = {
       searchNews() {
         let self = this;
         fetch('https://newsapi.org/v2/everything?q='+ self.searchTerm + '&language=en', {
-          headers: {'Authorization': 'Bearer ----'}
+          headers: {'Authorization': '9ee3b36c357c4566935a76191387af05'}
         })
         
         .then(function(response) {
